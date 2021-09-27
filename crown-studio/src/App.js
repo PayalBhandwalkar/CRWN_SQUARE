7f31360c54dcfd31b10541d4fa90989249405ffb
Payal Bhandwalkar <bhandwalkar.payal@gmail.com>
2021-09-27 21:02:36 +0530

    moved directory and shop to its own reducer, created respective selectors and updated the components

--- crown-studio/src/App.js
+++ crown-studio/src/App.js
@@ -52,24 +52,26 @@ class App extends React.Component {
        <Route exact path='/' component={Homepage}/>
        <Route path='/shop' component={ShopPage}/>
        <Route exact path='/checkout' component={CheckoutPage}/>
-       <Route exact path='/signin' 
-       render={() => 
-       this.props.currentUser ? (
-       <Redirect to='/' />
-       ) : (
-         <SignInAndSignUpPage />
-       )
-      }
-      />
+       <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
      </Switch>
     </div>
   );
 }
 }
 
-const mapStateToProps = ({ user }) => ({
+const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- crown-studio/src/components/custom-button/custom-button.component.jsx
+++ crown-studio/src/components/custom-button/custom-button.component.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ 
-    children, 
+
+const CustomButton = ({
+    children,
     isGoogleSignIn,
     inverted,
-     ...otherProps
-    }) => (
-    <button 
-    className={`${inverted ? 'inverted' : ''} ${
-    isGoogleSignIn ? 'google-sign-in' : ''
-    }custom-button`} 
-    { ...otherProps}
+    ...otherProps
+  }) => (
+    <button
+      className={`${inverted ? 'inverted' : ''} ${
+        isGoogleSignIn ? 'google-sign-in' : ''
+      } custom-button`}
+      {...otherProps}
     >
-    {children}
+      {children}
     </button>
-);
+  );
+  
 
 export default CustomButton;
